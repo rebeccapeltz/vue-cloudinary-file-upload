51d3195dd0f5d221af2b347a1c65dcb497a10851
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-11-06 20:46:11 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vue-cloudinary-file-upload
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="hello">
+    <h1>Upload to Cloudianry</h1>
+    <h3>Using Axios</h3>
+    <form v-on:submit.prevent="upload">
+      <p>
+        <file-select v-model="file"></file-select>
+      </p>
+      <p v-if="file">{{file.name}}</p>
+      <button type="submit">Upload</button>
+    </form>
+    <!-- <form enctype="multipart/form-data">
+      <input v-model="inputfiles" @change="upload" id="upload" type="file" accept="text/html" name="files[]" size="30" />
+    </form>-->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import FileSelect from "./FileSelect.vue";
+export default {
+  name: "HelloWorld",
+  components: {
+    FileSelect
+  },
+  data() {
+    return {
+      results: null,
+      errors: [],
+      file: null,
+      inputfiles: null,
+      cloudName: "picturecloud7",
+      preset: "bp_test_1",
+      tags: "browser-upload"
+    };
+  },
+  methods: {
+    upload: function(e) {
+      console.log(this.file.name);
+      let reader = new FileReader();
+
+      reader.addEventListener(
+        "load",
+        function() {
+          // this.showPreview = true;
+          // this.imagePreview = reader.result;
+          let fd = new FormData()
+          fd.append("upload_preset", this.preset)
+          fd.append("tags", this.tags); // Optional - add tag for image admin in Cloudinary
+          fd.append("file",  reader.result)
+          axios
+            .post(
+              `https://api.cloudinary.com/v1_1/${this.cloudName}/image/upload`,
+              fd
+            )
+            .then(response => {
+              this.results = response.data;
+              console.log(this.results);
+            })
+            .catch(error => {
+              this.errors.push(error);
+              console.log(this.error);
+            });
+        }.bind(this),
+        false
+      );
+
+      if (this.file) {
+        /*
+        Ensure the file is an image file.
+      */
+        if (/\.(jpe?g|png|gif)$/i.test(this.file.name)) {
+          /*
+          Fire the readAsDataURL method which will read the file in and
+          upon completion fire a 'load' event which we will listen to and
+          display the image in the preview.
+        */
+          reader.readAsDataURL(this.file);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+// https://alligator.io/vuejs/file-select-component/
